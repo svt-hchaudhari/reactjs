52150b7ee17d44f55ba1ad24b91c68e1bb512d43
Product Migrator <genbee@example.com>
2025-08-09 10:44:03 +0000

    Migration result task_id 5bc83d13-d334-4c27-93b3-e3b234ad9dba

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import H1Component from './H1Component';
+import DivContainer from './DivContainer';
+
+const App = () => {
+  return (
+    <DivContainer>
+      <H1Component />
+    </DivContainer>
+  );
+};
+
+export default App;
